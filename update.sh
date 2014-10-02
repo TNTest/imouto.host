1730394d87bd0697aac8e0678d6136ac52537dfd
tnt <tnt@bjj1.(none)>
2014-10-03 02:00:10 +0800

    update from imouto

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/tnt/imouto.host
 git pull
 wget https://www.dropbox.com/sh/lw0ljk3sllmimpz/AADvmg0wxOXHAtLQ9WhPlvAva/imouto.host.txt?dl=1 -O imouto.host.txt
 git commit -a -m "update from imouto"
